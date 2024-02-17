24b282a4f5459cc6b9913ab5eb0ee22906acb4a9
Tracey <morrist29@gmatc.matc.edu>
2024-02-17 16:31:00 -0600

    activity-1.0

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -9,10 +9,14 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Trays Gatsby Blog`,
+    description: `Welcome to the thunder dome.`,
+    author: `Tracey Morris`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    contact: {
+      name: `Tracey Morris`,
+      company: `Big Blogs LLC`
+    }
   },
   plugins: [
     `gatsby-plugin-image`,
