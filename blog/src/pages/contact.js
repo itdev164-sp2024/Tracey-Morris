5180ba59917ce8996f323609b38d06bc89b71bdd
Tracey <morrist29@gmatc.matc.edu>
2024-02-18 11:30:21 -0600

    activity-1.0

--- blog/src/pages/contact.js
+++ blog/src/pages/contact.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { graphql, Link } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 
-import { Layout } from '../components/layout'
+import Layout from '../components/layout'
 import Seo from '../components/seo'
 
 const Contact = ({ data }) => {
-    const { name, company,  } = data.site.siteMetadata.contact
+    const { name, company, } = data.site.siteMetadata.contact
     return (
         <Layout>
             <Seo title="Contact Us" />
@@ -17,7 +17,7 @@ const Contact = ({ data }) => {
             <div>{name}</div>
             <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
                 <StaticImage
-                src="../image/gatsby-icon.png"
+                src="../images/gatsby-icon.png"
                 width={300}
                 quality={95}
                 formats={["auto", "webp", "avif"]}
