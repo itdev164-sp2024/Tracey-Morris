fe639e1ecc683e8dc748169b88765480a57f7079
Tracey <morrist29@gmatc.matc.edu>
2024-02-28 00:56:28 -0600

    activity-2

--- blog/src/templates/blog-post.js
+++ blog/src/templates/blog-post.js
@@ -3,7 +3,7 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
 const BlogPost = ({ data }) => {
-  const { title } = data.contentfulBlogPost
+  const { title } = data.contentfulBlogPost;
 
   return (
     <Layout>
