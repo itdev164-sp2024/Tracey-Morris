a9aa263a15bf4d6ae1237e857507fe6bd4c19fdd
Tracey <morrist29@gmatc.matc.edu>
2024-02-27 22:18:45 -0600

    activity-2

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,37 +1,46 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
 
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-  },
-  {
-    text: "Examples",
-    url: "https://github.com/gatsbyjs/gatsby/tree/master/examples",
-    description:
-      "A collection of websites ranging from very basic to complex/complete that illustrate how to accomplish specific tasks within your Gatsby sites.",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Learn how to add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-  },
-]
+const IndexPage = ({ data }) => (
+  <Layout>
+    <Seo title="Home" />
+    <ul className={styles.list}>
+{
+  data.allContentfulBlogPost.edges.map(edge => (
+    <li key={edge.node.id}>
+      <Link to={edge.node.slug}>{edge.node.title}</Link>
+    </li>
+  ))
+}
+  </ul>
+ </Layout>
+)
+/**
+ * Head export to define metadata for the page
+ * 
+ * see: http://www.gatsbyjs.com/docs/reference/built-in-componets/gatsby-head/
+ */
+export const Head = () => <Seo title="Home" />
+
+export default IndexPage
+
+export const query = graphql`
+{
+  allContentfulBlogPost {
+    edges {
+      node {
+        id 
+        title 
+        slug
+      }
+    }
+  }
+}
+`
 
 const samplePageLinks = [
   {
