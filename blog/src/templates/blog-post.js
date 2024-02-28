6142efa220d5b11ccd9393f3a097cf0da72cb8d9
Tracey <morrist29@gmatc.matc.edu>
2024-02-28 00:06:40 -0600

    activity-2

--- blog/src/templates/blog-post.js
+++ blog/src/templates/blog-post.js
@@ -3,20 +3,19 @@ import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
 const BlogPost = ({ data }) => {
-    const { title } = data.contentfulBlogPost;
+    const { title } = data.contentfulBlogPost
 
     return (
         <Layout> 
             <h1>{title}</h1>
-            </Layout>
+        </Layout>
 
     )
 }
 
-export default BlogPost;
+export default BlogPost
 
 export const pageQuery = graphql`
-
 query BlogPostQuery($slug: String!) {
     contentfulBlogPost(slug: {eq: $slug}) {
         title
