85575a88adfd26fe00b55e432ec83540fec1c8d4
Tracey <morrist29@gmatc.matc.edu>
2024-02-28 00:45:38 -0600

    activity-2

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -7,20 +7,25 @@ import * as styles from "../components/index.module.css"
 
 const IndexPage = ({ data }) => (
   <Layout>
-    <Seo title="Home" />
+    <Seo title= "Home" />
     <ul className={styles.list}>
-  {data.allContentfulBlogPost.edges.map(edge => (
-    <li key={edge.node.id}>
-      <Link to={edge.node.slug}>{edge.node.title}</Link>
-    </li>
-  ))}
-  </ul>
- </Layout>
+      {
+        data.allContentfulBlogPost.edges.map(edge => (
+          <li key={edge.node.id}>
+            <Link to={edge.node.slug}>{edge.node.title}</Link>
+          </li>
+        ))
+
+      }
+    </ul>
+
+  </Layout>
 )
+
 /**
  * Head export to define metadata for the page
- * 
- * see: http://www.gatsbyjs.com/docs/reference/built-in-componets/gatsby-head/
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
 export const Head = () => <Seo title="Home" />
 
@@ -28,13 +33,14 @@ export default IndexPage
 
 export const query = graphql`
   {
-   allContentfulBlogPost {
-     edges {
+    allContentfulBlogPost {
+      edges {
         node {
-          id 
-          title 
-         slug
-       }
-     }
-   }
-}`
+          id
+          title
+          slug
+        }
+      }
+    }
+  }
+`
