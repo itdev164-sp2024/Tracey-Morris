d0f00041c86ed05012fe9c4bc98df90b4496a3a7
Tracey <morrist29@gmatc.matc.edu>
2024-03-10 21:59:59 -0500

    activtiy-3

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,13 +1,11 @@
-require ('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`
-})
-
 /**
  * Configure your Gatsby site with this file.
  *
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
  */
-
+require ('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+})
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
