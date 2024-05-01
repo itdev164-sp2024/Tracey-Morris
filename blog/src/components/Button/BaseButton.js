a48de7dbe437de4bb1186e091c32ba00bc1b675d
Tracey <morrist29@gmatc.matc.edu>
2024-05-01 15:31:01 -0500

    activity-8

---
+++ blog/src/components/Button/BaseButton.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Button } from 'rebass'
+import styled from 'styled-components'
+
+const Reset = styled(Button)`
+    background: none;
+    box-shadow: none;
+    font-weight: normal;
+    border-radius: 0;
+    cursor: pointer;
+`
+
+export const BaseButton = ({ Children, ...rest}) =>
+    <Reset {...rest}>{Children}</Reset>
+
+BaseButton.propTypes = {
+    children: PropTypes.node,isRequired
+}
