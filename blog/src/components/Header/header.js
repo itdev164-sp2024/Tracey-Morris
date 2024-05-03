626800c7b174cc2dd6bd546b7618f51bd4b6ef72
Tracey <morrist29@gmatc.matc.edu>
2024-05-03 04:16:11 -0500

    activity-8

--- blog/src/components/Header/header.js
+++ blog/src/components/Header/header.js
@@ -1,10 +1,10 @@
 import * as React from "react"
-import  PropTypes  from "prop-types"
+import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import styled from "styled-components"
 import { Search } from 'styled-icons/feather'
 import { H1 } from "../Heading"
-import { IconButton } from '../Button'
+import { IconButton } from "../Button"
 import { Section } from '../Section'
 
 const StyledHeader = styled.header`
@@ -16,24 +16,26 @@ const StyledHeader = styled.header`
   height: 50px;
   background: ${({ theme }) => theme.variants.header.primary.backgroundColor};
 `
+
 const StyledLink = styled(Link)`
   font-size: var(--font-sm);
   text-decoration: none;
   color: ${({ theme }) => theme.variants.header.primary.color};
 `
+
 const Header = ({ siteTitle }) => (
   <StyledHeader>
-  <Section width={11/12}>
-    <StyledLink to="/">
-      <H1>
-        {siteTitle}
-      </H1>
-    </StyledLink>
-  </Section>
-  <Section width={1/12}>
-    <IconButton icon={<Search />} variant='contrast' />
-  </Section>
-</StyledHeader>
+    <Section width={11/12}>
+      <StyledLink to="/">
+        <H1>
+          {siteTitle}
+        </H1>
+      </StyledLink>
+    </Section>
+    <Section width={1/12}>
+      <IconButton icon={<Search />} variant='contrast' />
+    </Section>
+  </StyledHeader>
 )
 
 Header.propTypes = {
