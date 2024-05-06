a4925f107afea9081bc9155890303c85a072a0d5
Tracey <morrist29@gmatc.matc.edu>
2024-05-06 12:00:46 -0500

    activity-9

--- blog/src/templates/blog-post.js
+++ blog/src/templates/blog-post.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image"
 import Layout from "../components/layout";
 import { H1 } from '../components/Heading'
 
 const BlogPost = ({ data }) => {
-  const { title, body } = data.contentfulBlogPost;
+  const { title, body, heroImage} = data.contentfulBlogPost;
 
   return (
     <Layout>
+        <GatsbyImage
+            image={heroImage.gatsbyImageData}
+        />
       <H1>{title}</H1>
       <div dangerouslySetInnerHTML={{__html: body.childMarkdownRemark.html}}></div>
     </Layout>
-  );
+  )
 }
 
-export default BlogPost;
+export default BlogPost
 
 export const pageQuery = graphql`
   query BlogPostQuery($slug: String!) {
@@ -26,6 +30,14 @@ export const pageQuery = graphql`
         html
         }
       }
+
+      heroImage {
+                gatsbyImageData(
+                    layout: CONSTRAINED
+                    placeholder: BLURRED
+                    width: 960
+                )
+            }
+	    }
     }
-  }
 `
